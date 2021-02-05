575af0deb90d3573ea3f06705ef82a100b60d31d
Jon-Nutman <jonnut730@gmail.com>
2021-02-05 09:10:03 -0500

    got it to console log the card number when clicking on said card.

---
+++ memory.js
@@ -0,0 +1,21 @@
+// make two sets of cards numbered 0-8 that way easier to match in future
+
+const arr = [0,1,2,3,4,5,6,7,8]
+.map((number) => `<button class="card">${number}</button>`)
+
+const arr2 = [0,1,2,3,4,5,6,7,8]
+.map((number) => `<button class="card">${number}</button>`)
+
+document.querySelector("#cardsContainer").innerHTML = (arr2).join('') + arr.join('')
+
+// listen for click on card
+
+document
+  .querySelector("#cardsContainer")
+  .addEventListener("click", function (e) {
+    e.preventDefault()
+let card = e.target.innerHTML
+console.log(card)
+    // card.style.display = 'none'
+
+  })
