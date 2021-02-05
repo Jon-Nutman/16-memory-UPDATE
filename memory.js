fa96a0cfe6d6e867b1f87334c7c60c32e0cfea8b
Jon-Nutman <jonnut730@gmail.com>
2021-02-05 09:12:51 -0500

    changed cards back to div

--- memory.js
+++ memory.js
@@ -1,10 +1,10 @@
 // make two sets of cards numbered 0-8 that way easier to match in future
 
 const arr = [0,1,2,3,4,5,6,7,8]
-.map((number) => `<button class="card">${number}</button>`)
+.map((number) => `<div class="card">${number}</div>`)
 
 const arr2 = [0,1,2,3,4,5,6,7,8]
-.map((number) => `<button class="card">${number}</button>`)
+.map((number) => `<div class="card">${number}</div>`)
 
 document.querySelector("#cardsContainer").innerHTML = (arr2).join('') + arr.join('')
 
@@ -16,6 +16,7 @@ document
     e.preventDefault()
 let card = e.target.innerHTML
 console.log(card)
-    // card.style.display = 'none'
+
+
 
   })
